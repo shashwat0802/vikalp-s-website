b55acec13e9316ada645089252c2de0aa14be410
Shashwat gupta <63670167+shashwat0802@users.noreply.github.com>
2020-08-08 01:56:55 +0530

    Update config.js
    use own creds for Firebase API

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,13 @@ import "firebase/firestore"
 import "firebase/auth"
 
 var firebaseConfig = {
-  apiKey: "AIzaSyBO8Gd1FAQHUzyIv6RIGoGdtsxtx00A_CE",
-  authDomain: "photo-website-52184.firebaseapp.com",
-  databaseURL: "https://photo-website-52184.firebaseio.com",
-  projectId: "photo-website-52184",
-  storageBucket: "photo-website-52184.appspot.com",
-  messagingSenderId: "102989316981",
-  appId: "1:102989316981:web:0e1d8a4bf01229ae83c93c",
+  apiKey: "xxx",
+  authDomain: "xxx",
+  databaseURL: "xxx",
+  projectId: "xxx",
+  storageBucket: "xxx",
+  messagingSenderId: "xxx",
+  appId: "xxx",
 }
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig)
